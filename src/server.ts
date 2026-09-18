3fe7c80b3307d74412acd5dbdeee8aef843e73e3
Élcio M. Fernandes <elciomf@proton.me>
2026-09-18 14:28:03 -0300

    feat: add routes for fetching CNAE sections, divisions, groups, classes, and subclasses

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import { createClient } from "redis";
 
 import Bucket from "./utils/bucket.ts";
+import fetcher from "./utils/request.ts";
 
 function example<T>(value: T): T {
   try {
@@ -43,6 +44,81 @@ fastify.post<{ Body: { data: unknown } }>("/", async (request, reply) => {
   const { data } = request.body;
   reply.send(example(data));
 });
+
+// Seção
+fastify.get<{ Params: { id: string } }>(
+  "/cnae/se/:id",
+  async (request, reply) => {
+    const { id } = request.params;
+
+    reply.send(
+      await fetcher(
+        `https://servicodados.ibge.gov.br/api/v2/cnae/secoes/${id}`,
+        "GET",
+      ),
+    );
+  },
+);
+
+// Divisão
+fastify.get<{ Params: { id: string } }>(
+  "/cnae/di/:id",
+  async (request, reply) => {
+    const { id } = request.params;
+
+    reply.send(
+      await fetcher(
+        `https://servicodados.ibge.gov.br/api/v2/cnae/divisoes/${id}`,
+        "GET",
+      ),
+    );
+  },
+);
+
+// Grupo
+fastify.get<{ Params: { id: string } }>(
+  "/cnae/gr/:id",
+  async (request, reply) => {
+    const { id } = request.params;
+
+    reply.send(
+      await fetcher(
+        `https://servicodados.ibge.gov.br/api/v2/cnae/grupos/${id}`,
+        "GET",
+      ),
+    );
+  },
+);
+
+// Classe
+fastify.get<{ Params: { id: string } }>(
+  "/cnae/cl/:id",
+  async (request, reply) => {
+    const { id } = request.params;
+
+    reply.send(
+      await fetcher(
+        `https://servicodados.ibge.gov.br/api/v2/cnae/classes/${id}`,
+        "GET",
+      ),
+    );
+  },
+);
+
+// Subclasse
+fastify.get<{ Params: { id: string } }>(
+  "/cnae/su/:id",
+  async (request, reply) => {
+    const { id } = request.params;
+
+    reply.send(
+      await fetcher(
+        `https://servicodados.ibge.gov.br/api/v2/cnae/subclasses/${id}`,
+        "GET",
+      ),
+    );
+  },
+);
 
 const startup = async () => {
   try {
